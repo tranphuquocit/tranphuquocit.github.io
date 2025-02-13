447dfcdb458715d7a728eca214c3ad5e16fba926
Zin <tranphuquocit@gmail.com>
2025-02-13 15:03:33 +0700

    update cv

--- frontend/functions.js
+++
@@ -1,9 +0,0 @@
-var header = document.getElementById("menuHeader");
-var btns = header.getElementsByClassName("menu-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("menu-active");
-  current[0].className = current[0].className.replace(" menu-active", "");
-  this.className += " menu-active";
-  });
-}
